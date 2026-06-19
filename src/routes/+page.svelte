e21ccdd3a9b10742d920a4d04a680ae4f082374b
ntillier <thelutrinos@gmail.com>
2026-06-19 16:19:36 +0200

    🚧 Meilleure page d'accueil

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 
 
 <!-- Présentation -->
-<div class="hero p-32 rounded-xl bg-linear-to-br from-emerald-500 to-emerald-400">
+
+<!-- from-emerald-500 to-emerald-400 -->
+<div class="hero p-32 rounded-xl bg-linear-to-br from-primary to-emerald-400">
 	<div class="hero-content text-center text-white">
 		<div class="max-w-2xl">
 			<h1 class="text-5xl md:text-6xl font-bold mb-6">SupRadar</h1>
@@ -9,7 +11,7 @@
 				Explorateur de formations sur Parcoursup
 			</p>
 			<div class="flex flex-col md:flex-row gap-4 justify-center">
-				<a href="/formations" class="btn btn-lg bg-white text-emerald-600 hover:bg-gray-100 border-0">
+				<a href="/formations" class="btn btn-lg bg-white text-black hover:bg-gray-100 border-0">
 					Découvrir les formations
 				</a>
 				<a href="/etablissements" class="btn btn-lg btn-outline border-white text-white hover:bg-white hover:text-emerald-600">
