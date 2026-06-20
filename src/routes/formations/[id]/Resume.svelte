bf3074a39052c83abcc3609faed390819bacac91
ntillier <thelutrinos@gmail.com>
2026-06-20 16:10:00 +0200

    🚧 Ajout d'une carte interactive

--- src/routes/formations/[id]/Resume.svelte
+++ src/routes/formations/[id]/Resume.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-    import { BarChart, defaultChartPadding } from "layerchart";
-    import { IndiceStats } from "$lib";
+    import { MapLibre, Marker } from "svelte-maplibre-gl";
 
     let { formation } = $props();
 </script>
 
 <section class="space-y-4">
     <h2 class="text-2xl font-semibold text-primary">Résumé</h2>
-    <div class="prose" style="max-width: none;">
-        Aucune description.
-    </div>
-</section> 
+    <div class="prose" style="max-width: none;">Aucune description.</div>
+
+    <MapLibre
+        class="h-100"
+        zoom={10}
+        center={[formation.longitude, formation.latitude]}
+        style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+    >
+        <Marker lnglat={[formation.longitude, formation.latitude]} />
+    </MapLibre>
+</section>
